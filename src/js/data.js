d5183744166de5f9925cf73f0e32970325d71960
gaposx <gonzaloparra@gmail.com>
2018-06-22 05:38:13 -0500

    Agregando logout

--- src/js/data.js
+++ src/js/data.js
@@ -1,40 +1,40 @@
 
-function sendMessage(event){
-  if (!event) { 
-    event = window.event; 
-  }
-  event.preventDefault(); // sometimes useful
+function sendMessage(event) {
+    if (!event) {
+        event = window.event;
+    }
+    event.preventDefault(); // sometimes useful
 
-  // Enter is pressed
-  if (event.keyCode == 13) { 
-    const database = firebase.database();
+    // Enter is pressed
+    if (event.keyCode == 13) {
+        const database = firebase.database();
 
-    const newMessage = { 
-      creationTime : firebase.database.ServerValue.TIMESTAMP,
-      creator : window.user.uid,
-      message : messageTextArea.value,
-      creatorAvatar : window.user.photoURL,
-      creatorName : user.displayName
-    };
-    const newMessageKey = database.ref().child('messages').push().key;
-    database.ref(`/messages/${newMessageKey}`).update(newMessage);
+        const newMessage = {
+            creationTime: firebase.database.ServerValue.TIMESTAMP,
+            creator: window.user.uid,
+            message: messageTextArea.value,
+            creatorAvatar: window.user.photoURL,
+            creatorName: user.displayName
+        };
+        const newMessageKey = database.ref().child('messages').push().key;
+        database.ref(`/messages/${newMessageKey}`).update(newMessage);
 
-    //Clear message box
-    messageTextArea.value = "";
-  }
+        //Clear message box
+        messageTextArea.value = "";
+    }
 }
 
-function startListeningNewMessages(callback){
-  firebase.database().ref('/.info/serverTimeOffset')
-  .once('value')
-  .then((data) => {
-    const nowTimestamp = data.val() + Date.now(); //Time limit to ignore old messages
-    var starCountRef = firebase.database().ref('messages/');
-    starCountRef.on('child_added', function(newMessage) {
-      if(newMessage.val().creationTime > nowTimestamp)
-        callback(newMessage.val());
-    });
-  }, (err) => {
-    console.error("Error while setting up message listener > "+err);
-  });
+function startListeningNewMessages(callback) {
+    firebase.database().ref('/.info/serverTimeOffset')
+        .once('value')
+        .then((data) => {
+            const nowTimestamp = data.val() + Date.now(); //Time limit to ignore old messages
+            var starCountRef = firebase.database().ref('messages/');
+            starCountRef.on('child_added', function (newMessage) {
+                if (newMessage.val().creationTime > nowTimestamp)
+                    callback(newMessage.val());
+            });
+        }, (err) => {
+            console.error("Error while setting up message listener > " + err);
+        });
 }
--- src/js/index.js
+++ src/js/index.js
@@ -1,47 +1,53 @@
 window.user = null;
 
-window.onload = function() {
-  var config = {
-    apiKey: "AIzaSyCjxtAm1i4s6v38hXEzB1zOGGVd_MLY9BI",
-    authDomain: "redsocial-798a2.firebaseapp.com",
-    databaseURL: "https://redsocial-798a2.firebaseio.com",
-    projectId: "redsocial-798a2",
-    storageBucket: "",
-    messagingSenderId: "822762995380"
-  };
-  firebase.initializeApp(config);
-  firebase.auth().onAuthStateChanged(function(user) {
-    if (user) {
-      // User is signed in.
-      const displayName = user.displayName;
-      const email = user.email;
-      const emailVerified = user.emailVerified;
-      const photoURL = user.photoURL;
-      const isAnonymous = user.isAnonymous;
-      const uid = user.uid;
-      const providerData = user.providerData;
-      
-      window.user = user;
-      console.log("User from github/firebase > "+JSON.stringify(user));
+window.onload = function () {
+    var config = {
+        apiKey: "AIzaSyCjxtAm1i4s6v38hXEzB1zOGGVd_MLY9BI",
+        authDomain: "redsocial-798a2.firebaseapp.com",
+        databaseURL: "https://redsocial-798a2.firebaseio.com",
+        projectId: "redsocial-798a2",
+        storageBucket: "",
+        messagingSenderId: "822762995380"
+    };
+    firebase.initializeApp(config);
+    firebase.auth().onAuthStateChanged(function (user) {
+        if (user) {
+            // User is signed in.
+            const displayName = user.displayName;
+            const email = user.email;
+            const emailVerified = user.emailVerified;
+            const photoURL = user.photoURL;
+            const isAnonymous = user.isAnonymous;
+            const uid = user.uid;
+            const providerData = user.providerData;
 
-      changeUIToOnlineMode();
-    } else {
-      console.log("User not signed in...");
-      changeUIToOfflineMode();
-    }
-  });
+            window.user = user;
+            console.log("User from github/firebase > " + JSON.stringify(user));
 
-  //Setup data listeners
+            changeUIToOnlineMode();
+        } else {
+            console.log("User not signed in...");
+            changeUIToOfflineMode();
+        }
+    });
 
-  startListeningNewMessages((newMessage)=>{ //Callback for new messages
-    const availableHeight = messageSpace.offsetHeight - 200;
-    const availableWidth = messageSpace.offsetWidth - 200;
+    $('a[data-toggle="pill"]').on('shown.bs.tab', e => {
+        if (e.target.id === 'v-pills-logout-tab') {
+            logout();
+        }
+    });
 
-    console.log("New message > "+newMessage.message);
-    const newMessageNode = document.createElement("div");
-    newMessageNode.id = `message_${newMessage.creationTime}`;
-    newMessageNode.className = `message`;
-    newMessageNode.innerHTML = `
+    //Setup data listeners
+
+    startListeningNewMessages((newMessage) => { //Callback for new messages
+        const availableHeight = messageSpace.offsetHeight - 200;
+        const availableWidth = messageSpace.offsetWidth - 200;
+
+        console.log("New message > " + newMessage.message);
+        const newMessageNode = document.createElement("div");
+        newMessageNode.id = `message_${newMessage.creationTime}`;
+        newMessageNode.className = `message`;
+        newMessageNode.innerHTML = `
     <div class="card border-info mb-3" style="max-width: 18rem;">
       <div class="card-header">
         ${newMessage.creatorName}
@@ -57,79 +63,83 @@ window.onload = function() {
     </div>
     `;
 
-    newMessageNode.style.top = availableHeight * Math.random();
-    newMessageNode.style.left = availableWidth * Math.random();
+        newMessageNode.style.top = availableHeight * Math.random();
+        newMessageNode.style.left = availableWidth * Math.random();
 
-    messageSpace.appendChild(newMessageNode);
-  });
-    
+        messageSpace.appendChild(newMessageNode);
+    });
 
-  /*TODO: Log-out
-  firebase.auth().signOut().then(function() {
-    // Sign-out successful.
-  }).catch(function(error) {
-    // An error happened.
-  });
-  */
 
-  startAnimationLoop();
+
+
+    startAnimationLoop();
 };
 
-function login(){
-  const provider = new firebase.auth.GithubAuthProvider();
-    //$('#myTab a[href="#profile"]').tab('show')
-    firebase.auth().signInWithPopup(provider).then(function(result) {
-      const token = result.credential.accessToken;
-      const user = result.user; 
-      changeUIToOnlineMode();
-    }).catch(function(error) {
-      const errorCode = error.code;
-      const errorMessage = error.message;
-      const email = error.email;
-      const credential = error.credential;
+const logout = () => {
+    // TODO: Log-out
+    firebase.auth().signOut().then(function () {
+        // Sign-out successful.
+    }).catch(function (error) {
+        // An error happened.
+    });
 
-      console.error("Login error > "+errorMessage);
-      //TODO: implementar login error en interfaz
+}
+
+function login() {
+    const provider = new firebase.auth.GithubAuthProvider();
+    //$('#myTab a[href="#profile"]').tab('show')
+    firebase.auth().signInWithPopup(provider).then(function (result) {
+        const token = result.credential.accessToken;
+        const user = result.user;
+        changeUIToOnlineMode();
+    }).catch(function (error) {
+        const errorCode = error.code;
+        const errorMessage = error.message;
+        const email = error.email;
+        const credential = error.credential;
+
+        console.error("Login error > " + errorMessage);
+        //TODO: implementar login error en interfaz
     });
 }
 
-function changeUIToOfflineMode(){
-  $("#v-pills-home-tab").hide();
-  $("#v-pills-friends-tab").hide();
-  $("#v-pills-logout-tab").hide();
-  $("#v-pills-logout-tab").show();
-  $("#v-pills-login-tab").tab('show');
+function changeUIToOfflineMode() {
+    $("#v-pills-home-tab").hide();
+    $("#v-pills-friends-tab").hide();
+    $("#v-pills-logout-tab").hide();
+    $("#v-pills-logout-tab").hide();
+    $("#v-pills-login-tab").tab('show');
 }
 
-function changeUIToOnlineMode(){
-  $("#v-pills-home-tab").show();
-  $("#v-pills-friends-tab").show();
-  $("#v-pills-logout-tab").show();
-  $("#v-pills-login-tab").hide();
-  $("#v-pills-home-tab").tab('show');
+function changeUIToOnlineMode() {
+    $("#v-pills-home-tab").show();
+    $("#v-pills-friends-tab").show();
+    $("#v-pills-logout-tab").show();
+    $("#v-pills-login-tab").hide();
+    $("#v-pills-home-tab").tab('show');
 }
 
 //Setup animation loop
-function startAnimationLoop(){
-  setInterval(()=>{
-    const messages = Object.values(document.getElementsByClassName('message'));
+function startAnimationLoop() {
+    setInterval(() => {
+        const messages = Object.values(document.getElementsByClassName('message'));
 
-    messages.forEach(message => {
-      if(!message.style.opacity){
-        message.style.opacity = 1;
-      }
-      const newOpacity = message.style.opacity - 0.001;
-      message.style.opacity = newOpacity > 0 ? newOpacity : 0;
+        messages.forEach(message => {
+            if (!message.style.opacity) {
+                message.style.opacity = 1;
+            }
+            const newOpacity = message.style.opacity - 0.001;
+            message.style.opacity = newOpacity > 0 ? newOpacity : 0;
 
-      if(message.scale){
-        message.scale = message.scale - 0.001;
-        if(message.scale < 0){
-          message.scale = 0;
-        }
-        message.style.transform = `scale(${message.scale},${message.scale})`;
-      }else{
-        message.scale = 1;
-      }
-    });
-  }, 20);
+            if (message.scale) {
+                message.scale = message.scale - 0.001;
+                if (message.scale < 0) {
+                    message.scale = 0;
+                }
+                message.style.transform = `scale(${message.scale},${message.scale})`;
+            } else {
+                message.scale = 1;
+            }
+        });
+    }, 20);
 }
