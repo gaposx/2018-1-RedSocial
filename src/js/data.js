156f93cbe23f0f9f3c19aa60df76befc8c8d77e7
Fabián Alexis Bravo Abarca <fabian.bravo.abarca@gmail.com>
2018-06-21 21:17:38 -0400

    Errores de formato

--- src/js/data.js
+++ src/js/data.js
@@ -1,25 +1,25 @@
 
 function sendMessage(event){
-    if (!event) { 
-        event = window.event; 
-    }
-    event.preventDefault(); // sometimes useful
-  
-    // Enter is pressed
-    if (event.keyCode == 13) { 
-      const database = firebase.database();
-  
-      const newMessage = { 
-        creationTime : firebase.database.ServerValue.TIMESTAMP,
-        creator : window.user.uid,
-        message : messageTextArea.value,
-        creatorAvatar : window.user.photoURL,
-        creatorName : user.displayName
-      };
-      const newMessageKey = database.ref().child('messages').push().key;
-      database.ref(`/messages/${newMessageKey}`).update(newMessage);
-  
-      //Clear message box
-      messageTextArea.value = "";
-    }
+  if (!event) { 
+    event = window.event; 
   }
+  event.preventDefault(); // sometimes useful
+
+  // Enter is pressed
+  if (event.keyCode == 13) { 
+    const database = firebase.database();
+
+    const newMessage = { 
+      creationTime : firebase.database.ServerValue.TIMESTAMP,
+      creator : window.user.uid,
+      message : messageTextArea.value,
+      creatorAvatar : window.user.photoURL,
+      creatorName : user.displayName
+    };
+    const newMessageKey = database.ref().child('messages').push().key;
+    database.ref(`/messages/${newMessageKey}`).update(newMessage);
+
+    //Clear message box
+    messageTextArea.value = "";
+  }
+}
